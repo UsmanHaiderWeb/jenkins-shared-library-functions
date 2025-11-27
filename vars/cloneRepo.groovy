3a55b05fa4b0ca248e4213a33a85c7c77dfa80ff
UsmanHaiderWeb <usmanhaiderweb2005@gmail.com>
2025-11-27 14:16:24 +0500

    Update cloneRepo.groovy

--- vars/cloneRepo.groovy
+++ vars/cloneRepo.groovy
@@ -1,4 +1,7 @@
 def call(String repoUrl, String branch='main', String targetDir, String githubPat) {
     sh "rm -rf ${targetDir}"
-    sh "git clone --depth=1 https://${githubPat}@${repoUrl.replace('https://','')} ${targetDir}"
+
+    sh """
+        git clone --depth=1 --branch=${branch} https://${githubPat}@${repoUrl.replace('https://','')} ${targetDir}
+    """
 }
