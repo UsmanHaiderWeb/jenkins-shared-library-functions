50609c145b506f4a937e9e7e8680b4f648d2b119
UsmanHaiderWeb <usmanhaiderweb2005@gmail.com>
2025-11-27 12:59:51 +0500

    Update buildDocker.groovy

--- vars/buildDocker.groovy
+++ vars/buildDocker.groovy
@@ -1,7 +1,7 @@
 def call(String imageName, String tag, String contextDir) {
     dir(contextDir) {
         sh """
-            docker build -t ${imageName}:${tag} .
+            docker build -t ${imageName}:${tag} --no-cache .
             docker tag ${imageName}:${tag} ${imageName}:latest
         """
     }
